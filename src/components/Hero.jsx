38ae8706267c5ad77b804a664973b2a49390a7cb
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2024-03-01 16:57:32 +0200

    added the first part of hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import Section from './Section'
+import { curve } from '../assets'
+import Button from './Button'
+
+function Hero() {
+    return (
+        <Section
+            className="pt-[12rem] -mt-[5.25rem]"
+            crosses
+            crossesOffset="lg:translate-y-[5.25rem]"
+            customPaddings
+            id="hero"
+        >
+            <div className=' container relative'>
+                <div className=' relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] 
+        md:mb-20 lg:mb-[6rem]'>
+                    <h1 className="h1 mb-6">Siphamandla Mdletshe
+                        <span className="inline-block relative">
+                            Full Stack Software Developer{" "}
+                            <img
+                                src={curve}
+                                className="absolute top-full left-0 w-full xl:-mt-2"
+                                width={624}
+                                height={28}
+                                alt="Curve"
+                            />
+                        </span>
+                    </h1>
+                    <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+                        I have a passion for problem-solving, innovation, and creating high-quality products.
+                        Proficient in multiple programming languages and experienced in various
+                        software development methodologies, always eager to learn and collaborate
+                        on meaningful projects.
+                    </p>
+                    <Button href="/pricing" white>
+                        My Resume
+                    </Button>
+                </div>
+            </div>
+        </Section>
+    )
+}
+
+export default Hero
