082e8fc27aba20ba5a5a7db42ada9e787d3cd2a6
smdletsh021 <smdletsh021@student.wethinkcode.co.za>
2024-03-02 13:57:28 +0200

    added the first part of hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,8 +24,8 @@ function Hero() {
                 <div className=' relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] 
         md:mb-20 lg:mb-[6rem]'>
                     <h1 className="h1 mb-6">Siphamandla Mdletshe
-                    <br />
-                        <span className="inline-block relative">
+                        <br />
+                        <span className="inline-block relative text-[20px] md:h1">
                             <Typewriter
                                 options={{
                                     strings: core_skills,
